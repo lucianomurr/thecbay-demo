8a6e3165d02c7271089e1355c09d1a15bb9d3bad
Luciano Murruni <luciano.murruni@mac-006-2.homenet.telecomitalia.it>
2024-05-15 17:26:28 +0200

    feat: add vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,9 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     analog({
+      nitro: {
+        preset: 'vercel',
+      },
       static: true,
       prerender: {
         routes: async () => {

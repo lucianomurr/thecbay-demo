e86f4528ca4692dd6260cafc6457dc20eb0325be
Luciano Murruni <luciano.murruni@gmail.com>
2024-05-17 15:36:35 +0200

    feat: add animation withViewTransitions() when route change

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,13 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideFileRouter } from '@analogjs/router';
+import { provideFileRouter, routes } from '@analogjs/router';
 import { provideContent, withMarkdownRenderer } from '@analogjs/content';
+import { RouterFeatures, withViewTransitions } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideFileRouter(),
+    provideFileRouter(withViewTransitions()),
     provideHttpClient(withFetch()),
     provideClientHydration(),    
     provideContent(withMarkdownRenderer())
